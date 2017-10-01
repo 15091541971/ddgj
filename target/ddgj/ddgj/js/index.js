d8827fb83173cc60778367b8bb6f0aa07d8ef040
王伟 <1198275521@qq.com>
2017-10-02 00:51:54 +0800

    mybatis 和shiro例子

--- target/ddgj/ddgj/js/index.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-	$(".middle_nav>ul>li").mouseover(function(){
-		console.log($(".middle_nav>ul>li").length)
-		$(".middle_nav>ul>li").removeClass("middle_nav-spe").eq($(this).index()).addClass("middle_nav-spe");
-	})
-})
--- target/ddgj/js/base.js
+++
@@ -1,9 +0,0 @@
-
-$(function(){
-	$(".nav_unit1").click(function(){
-		$(".nav_fab").css("display","block");
-		setTimeout(function(){
-			$(".nav_fab").css("display","none");
-		},1500)
-	})
-})
